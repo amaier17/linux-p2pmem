07502cc74609994c6f12fe984a23e0017d740abb
Andrew Maier <Andrew.Maier@eideticom.com>
2018-09-17 11:48:57 -0600

    Fixed compile issue

--- drivers/nvme/host/pci.c
+++ drivers/nvme/host/pci.c
@@ -312,7 +312,14 @@ static bool nvme_dbbuf_update_and_check_event(u16 value, u32 *dbbuf_db,
 			return false;
 	}
     else
-        dev_warn(dev->ctrl.device, "I didn't run mb or wmb\n");
+    {
+        static int i=0;
+        if (i % 65536 == 0)
+        {
+            printk(KERN_DEBUG "I didn't run mb or wmb\n");
+            ++i;
+        }
+    }
 
 	return true;
 }
